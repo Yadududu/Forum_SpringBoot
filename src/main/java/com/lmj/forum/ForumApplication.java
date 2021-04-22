37bb84265d106d33a34ba7010e86af271b9e2976
lmj <601674759@qq.com>
2021-04-22 17:57:46 +0800

    修改打包方式,从jar改为war

--- src/main/java/com/lmj/forum/ForumApplication.java
+++ src/main/java/com/lmj/forum/ForumApplication.java
@@ -3,15 +3,21 @@ package com.lmj.forum;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //使用MapperScan批量扫描所有的Mapper接口；
 @MapperScan(value = "com.lmj.forum.dao")
 @SpringBootApplication
-public class ForumApplication {
+public class ForumApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ForumApplication.class);
+    }
 }
